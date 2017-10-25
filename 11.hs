9b48ba544d073af71ab8e1ce57562aecfeedc86d
Sungjin Han <meinside@gmail.com>
2017-10-25 17:08:22 +0900

    Add 11.hs

---
+++ 11.hs
@@ -0,0 +1,38 @@
+{--
+https://wiki.haskell.org/99_questions/11_to_20
+
+(*) Modified run-length encoding.
+
+Modify the result of problem 10 in such a way that if an element has no duplicates it is simply copied into the result list. Only elements with duplicates are transferred as (N E) lists.
+
+Example in Haskell:
+
+P11> encodeModified "aaaabccaadeeee"
+[Multiple 4 'a',Single 'b',Multiple 2 'c', Multiple 2 'a',Single 'd',Multiple 4 'e']
+
+-- https://wiki.haskell.org/99_questions/Solutions/11
+--}
+--
+data Element a
+  = Multiple Int
+             a
+  | Single a
+  deriving (Show)
+
+--
+-- (using 'encode' from 10.hs)
+encode :: (Eq a) => [a] -> [(Int, a)]
+encode = foldr f []
+  where
+    f v [] = [(1, v)]
+    f v (x:xs)
+      | snd x == v = (fst x + 1, v) : xs
+      | otherwise = (1, v) : x : xs
+
+-- XXX - 'pattern matching' seems to be
+--       the only(?) way of accessing unnamed members of data struct...
+encodeModified :: (Eq a) => [a] -> [Element a]
+encodeModified xs = map f $ encode xs
+  where
+    f (1, v) = Single v
+    f (n, v) = Multiple n v
